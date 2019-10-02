124455f066e2e210bc7d278c5cefddb2bb2f03ad
fsugiyama <fsugiyama@pp>
2019-10-02 17:29:21 +0900

    index.html modified

--- src/entity/item.py
+++ src/entity/item.py
@@ -3,8 +3,15 @@ import datetime
 
 class Item(object):
 
-    def __init__(self, date, inout, name):
+    def __init__(self, date, workType="", inTime=None, outTime=None, workContent="", memo=""):
         self.date=date
-        self.dateStr=self.date.strftime("%Y/%m/%d %H:%M:%S")
-        self.inout=inout
-        self.name=name
+        self.dateStr=self.date.strftime("%Y/%m/%d")
+        self.workType=workType
+        self.inTime=inTime
+        if self.inTime is not None:
+            self.inTimeStr=self.inTime.strftime("%H:%M:%S")
+        self.outTime=outTime
+        if self.outTime is not None:
+            self.outTimeStr=self.outTime.strftime("%H:%M:%S")
+        self.workContent=workContent
+        self.memo=memo
--- src/managementService.py
+++ src/managementService.py
@@ -9,10 +9,8 @@ class ManagementService(object):
 
     def __init__(self):
         self.itemList = [
-            Item(datetime.datetime(2019, 8, 28, 9, 55, 28), "出勤", "杉山"),
-            Item(datetime.datetime(2019, 8, 28, 19, 0, 0), "退勤", "杉山"),
-            Item(datetime.datetime.today(), "出勤", "上野"),
-            Item(datetime.datetime.today() + datetime.timedelta(hours=8), "退勤", "上野")
+            Item(datetime.date(2019, 8, 28), "", datetime.time(9, 55, 28), datetime.time(18, 0, 28)),
+            Item(datetime.date(2019, 8, 29), "", datetime.time(9, 55, 28), datetime.time(18, 0, 28))
             ]
 
     def searchItemList(self):
