37b28db57fddab201bb62aaee66dfa748621d578
root <root@tueno-web01.us-central1-a.c.t-ueno.internal>
2019-11-27 11:39:40 +0900

    index.html modified

--- src/entity/item.py
+++ src/entity/item.py
@@ -3,8 +3,11 @@ import datetime
 
 class Item(object):
 
-    def __init__(self, date, inout, name):
+    def __init__(self, date, category, inttime, outtime, content, memo):
         self.date=date
         self.dateStr=self.date.strftime("%Y/%m/%d %H:%M:%S")
-        self.inout=inout
-        self.name=name
+        self.category=category
+        self.inttime=inttime
+        self.outtime=outtime
+        self.content=content
+        self.memo=memo
--- src/managementService.py
+++ src/managementService.py
@@ -9,10 +9,7 @@ class ManagementService(object):
 
     def __init__(self):
         self.itemList = [
-            Item(datetime.datetime(2019, 8, 28, 9, 55, 28), "出勤", "杉山"),
-            Item(datetime.datetime(2019, 8, 28, 19, 0, 0), "退勤", "杉山"),
-            Item(datetime.datetime.today(), "出勤", "上野"),
-            Item(datetime.datetime.today() + datetime.timedelta(hours=8), "退勤", "上野")
+            Item(datetime.datetime(2019, 8, 28, 9, 55, 28), "遅刻", datetime.datetime(2019, 8, 28, 9, 55, 28), datetime.datetime(2019, 8, 28, 17, 55, 28), "出勤", "memo")
             ]
 
     def searchItemList(self):
