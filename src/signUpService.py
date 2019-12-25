b8a448d67dc57ce6aade02bb5447d68afa21330d
product <product>
2019-12-25 10:13:54 +0900

    aa

--- src/signUpService.py
+++ src/signUpService.py
@@ -1,8 +1,6 @@
 # coding: UTF-8
 import binascii
-import nfc
 import requests
-import hashlib
 import mysql.connector as mydb
 import configparser
 import os
@@ -20,25 +18,27 @@ class SignUpService(object):
     def registerAction(self, employeeNumber, username, password):
 
         # setting.iniから接続情報の取得
-        CONFIG_FILE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)) + "\\setting.ini")
+        CONFIG_FILE = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + r"/setting.ini"
 
-        print(CONFIG_FILE)
+        print("MYSQL接続設定ファイル : " + CONFIG_FILE)
 
-        # conf = configparser.SafeConfigParser()
-        # conf.read(CONFIG_FILE)
+        conf = configparser.SafeConfigParser()
+        conf.read(CONFIG_FILE)
 
-        # USERNAME = conf.get('connection', 'user')
-        # PASSWORD = conf.get('connection', 'password')
-        # SERVER   = conf.get('connection', 'server')
-        # DATABASE = conf.get('connection', 'database')
+        HOST     = conf.get('connection', 'host')
+        PORT     = conf.get('connection', 'port')
+        USERNAME = conf.get('connection', 'user')
+        PASSWORD = conf.get('connection', 'password')
+        DATABASE = conf.get('connection', 'database')
 
         # 接続する
         conn = mydb.connect(
-            host="35.209.236.130",
-            port='3306',
-            user='py',
-            passwd='pyPY0000/',
-            db='attendance')
+            host=HOST,
+            port=PORT,
+            user=USERNAME,
+            passwd=PASSWORD,
+            db=DATABASE,
+	    auth_plugin='mysql_native_password')
 
         # コネクションが切れた時に再接続してくれるよう設定
         # conn.ping(reconnect=True)
@@ -49,16 +49,15 @@ class SignUpService(object):
         # カーソルを取得する
         cur = conn.cursor()
 
-        # SQL（データベースを操作するコマンド）を実行する
-        # レコードの登録
-        sql = 'insert into m_user (user_no, name, password) values (%s, %s, %s)'
-        c.execute(sql, (employeeNumber, username, password))  # 1件のみ
-        # datas = [
-        #     (2, 'foo'),
-        #     (3, 'bar')
-        # ]
-        # c.executemany(sql, datas)    # 複数件
-        # print('\n* レコードを3件登録\n')
+        try:
+            # SQL（データベースを操作するコマンド）を実行する
+            # レコードの登録
+            sql = 'insert into m_user (user_no, name, password, start_time, end_time, create_user, update_user) values (%s, %s, %s, "1000-01-01 00:00:00.000000", "9999-01-01 00:00:00.000000", "admin", "admin")'
+            cur.execute(sql, (employeeNumber, username, password))  # 1件のみ
+            conn.commit()
+        except:
+            conn.rollback()
+            raise
 
         cur.close
         conn.close
